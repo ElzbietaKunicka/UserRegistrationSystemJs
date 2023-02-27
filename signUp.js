7ed81fcfdc93ea2266b945be43f3b948999c368c
elzzzbieta <elzbieta.petkevic@gmail.com>
2023-02-27 17:18:28 +0200

    Change signup function

--- signUp.js
+++ signUp.js
@@ -1,11 +1,17 @@
 const form = document.querySelector("#signUp");
+function reset() {
+  window.location.reload();
+}
+
+function backToLoginForm() {
+  window.location.href = "./index.html";
+}
 
 form.addEventListener("submit", async e => {
   e.preventDefault();
   const userName = document.querySelector("#userName").value;
   const userPassword = document.querySelector("#password").value;
-  console.log(userName);
-
+  
   try {
     const response = await fetch("http://localhost:5200/api/Accounts/SignUp", {
       method: "POST",
@@ -18,22 +24,24 @@ form.addEventListener("submit", async e => {
       })
     });
     const result = await response.text();
+    
+    const exceptionString = "A user with this username already exists.";
+    const resultMessageContainer = document.getElementById("Messages");
+    const messageParagraph = document.createElement("p");
 
-    if (result && result.length) {
-      const resultMessageContainer = document.getElementById("Messages");
-      const messageParagraph = document.createElement("p");
+    if (result === "A user with this username already exists.") {
+      messageParagraph.style.color = "#ff3939";
+      messageParagraph.textContent =
+        "A user with this username already exists. Try again";
+      setTimeout(reset, 5000);
+    } else {
+      messageParagraph.style.color = "#ff3998";
       messageParagraph.textContent = `System account is created. User name is ${result}.
-             Now you can Login`;
-      messageParagraph.style.color = "#555555";
-      resultMessageContainer.append(messageParagraph);
+      Now you can Login`;
+      setTimeout(backToLoginForm, 5000);
     }
-
-    setTimeout(time, 5000);
-    function time() {
-      window.location.href = "./index.html";
-    }
+    resultMessageContainer.append(messageParagraph);
   } catch (error) {
     console.error(error);
   }
 });
-
